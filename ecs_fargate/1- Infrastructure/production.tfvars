2eb9805c4177ea42d37d4ffa527269cf76f09d37
umartahir93 <umartahir@live.com>
2020-04-04 20:04:19 +0500

    modified infrastructure files

--- ecs_fargate/1- Infrastructure/production.tfvars
+++ ecs_fargate/1- Infrastructure/production.tfvars
@@ -1,4 +1,5 @@
 
+region= "us-east-1"
 vpc_cidr = "10.0.0.0/16"
 
 public_subnet_1_cidr = "10.0.1.0/24"
--- ecs_fargate/1- Infrastructure/vpc.tf
+++ ecs_fargate/1- Infrastructure/vpc.tf
@@ -1,12 +1,13 @@
+#VPC Resource
 resource "aws_vpc" "production-vpc" {
   cidr_block           = var.vpc_cidr
-  enable_dns_hostnames = true
 
   tags = {
     Name = "Production-VPC"
   }
 }
 
+#Public Subnet #1
 resource "aws_subnet" "public-subnet-1" {
   cidr_block        = var.public_subnet_1_cidr
   vpc_id            = aws_vpc.production-vpc.id
@@ -17,6 +18,7 @@ resource "aws_subnet" "public-subnet-1" {
   }
 }
 
+#Public Subnet #2
 resource "aws_subnet" "public-subnet-2" {
   cidr_block        = var.public_subnet_2_cidr
   vpc_id            = aws_vpc.production-vpc.id
@@ -27,6 +29,7 @@ resource "aws_subnet" "public-subnet-2" {
   }
 }
 
+#Public Subnet #3
 resource "aws_subnet" "public-subnet-3" {
   cidr_block        = var.public_subnet_3_cidr
   vpc_id            = aws_vpc.production-vpc.id
@@ -37,6 +40,7 @@ resource "aws_subnet" "public-subnet-3" {
   }
 }
 
+#Private Subnet #1
 resource "aws_subnet" "private-subnet-1" {
   cidr_block        = var.private_subnet_1_cidr
   vpc_id            = aws_vpc.production-vpc.id
@@ -47,6 +51,7 @@ resource "aws_subnet" "private-subnet-1" {
   }
 }
 
+#Private Subnet #2
 resource "aws_subnet" "private-subnet-2" {
   cidr_block        = var.private_subnet_2_cidr
   vpc_id            = aws_vpc.production-vpc.id
@@ -57,6 +62,7 @@ resource "aws_subnet" "private-subnet-2" {
   }
 }
 
+#Private Subnet #3
 resource "aws_subnet" "private-subnet-3" {
   vpc_id            = aws_vpc.production-vpc.id
   cidr_block        = var.private_subnet_3_cidr
@@ -67,6 +73,16 @@ resource "aws_subnet" "private-subnet-3" {
   }
 }
 
+#Internet Gateway
+resource "aws_internet_gateway" "production-igw" {
+  vpc_id = aws_vpc.production-vpc.id
+
+  tags = {
+    Name = "Production-IGW"
+  }
+}
+
+#Create a route table 
 resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.production-vpc.id
   tags ={
@@ -74,6 +90,35 @@ resource "aws_route_table" "public-route-table" {
   }
 }
 
+#Referenced the Internet gateway with this route resource
+resource "aws_route" "public-internet-gw-route" {
+  route_table_id         = aws_route_table.public-route-table.id
+  gateway_id             = aws_internet_gateway.production-igw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+#Associate the routing table to the public subnet
+resource "aws_route_table_association" "public-subnet-1-association" {
+  route_table_id = aws_route_table.public-route-table.id
+  subnet_id      = aws_subnet.public-subnet-1.id
+}
+
+
+#Associate the routing table to the public subnet
+resource "aws_route_table_association" "public-subnet-2-association" {
+  route_table_id = aws_route_table.public-route-table.id
+  subnet_id      = aws_subnet.public-subnet-2.id
+}
+
+
+#Associate the routing table to the public subnet
+resource "aws_route_table_association" "public-subnet-3-association" {
+  route_table_id = aws_route_table.public-route-table.id
+  subnet_id      = aws_subnet.public-subnet-3.id
+}
+
+
+#Create a route table 
 resource "aws_route_table" "private-route-table" {
   vpc_id = aws_vpc.production-vpc.id
 
@@ -82,42 +127,35 @@ resource "aws_route_table" "private-route-table" {
   }
 }
 
-resource "aws_route_table_association" "public-subnet-1-association" {
-  route_table_id = aws_route_table.public-route-table.id
-  subnet_id      = aws_subnet.public-subnet-1.id
-}
-
-resource "aws_route_table_association" "public-subnet-2-association" {
-  route_table_id = aws_route_table.public-route-table.id
-  subnet_id      = aws_subnet.public-subnet-2.id
-}
-
-resource "aws_route_table_association" "public-subnet-3-association" {
-  route_table_id = aws_route_table.public-route-table.id
-  subnet_id      = aws_subnet.public-subnet-3.id
-}
-
+#Associate the routing table to the private subnet
 resource "aws_route_table_association" "private-subnet-1-assocaiation" {
   route_table_id = aws_route_table.private-route-table.id
   subnet_id      = aws_subnet.private-subnet-1.id
 }
 
+
+#Associate the routing table to the private subnet
 resource "aws_route_table_association" "private-subnet-2-assocaiation" {
   route_table_id = aws_route_table.private-route-table.id
   subnet_id      = aws_subnet.private-subnet-2.id
 }
 
+
+#Associate the routing table to the private subnet
 resource "aws_route_table_association" "private-subnet-3-assocaiation" {
   route_table_id = aws_route_table.private-route-table.id
   subnet_id      = aws_subnet.private-subnet-3.id
 }
 
+
+#Create Elastic ip
 resource "aws_eip" "elastic-ip-for-nat-gw" {
   vpc                       = true
   associate_with_private_ip = "10.0.0.5"
 
 }
 
+#Create a nat gateway and place it into public subnet
 resource "aws_nat_gateway" "nat-gw" {
   allocation_id = aws_eip.elastic-ip-for-nat-gw.id
   subnet_id     = aws_subnet.public-subnet-1.id
@@ -130,41 +168,13 @@ resource "aws_nat_gateway" "nat-gw" {
 
 }
 
+#Add route to nat gateway in private subnet
 resource "aws_route" "nat_gw_route" {
   route_table_id         = aws_route_table.private-route-table.id
   nat_gateway_id         = aws_nat_gateway.nat-gw.id
   destination_cidr_block = "0.0.0.0/0"
 
 }
-
-resource "aws_internet_gateway" "production-igw" {
-  vpc_id = aws_vpc.production-vpc.id
-
-  tags = {
-    Name = "Production-IGW"
-  }
-
-}
-
-resource "aws_route" "public-internet-gw-route" {
-  route_table_id         = aws_route_table.public-route-table.id
-  gateway_id             = aws_internet_gateway.production-igw.id
-  destination_cidr_block = "0.0.0.0/0"
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
