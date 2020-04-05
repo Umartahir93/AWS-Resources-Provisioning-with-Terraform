8d5d1116a013f9e17143b9feb325bb5850eafd72
umartahir93 <umartahir@live.com>
2020-04-05 17:12:00 +0500

    now service can be created at this point

--- ecs_fargate/1- Infrastructure/outputs.tf
+++ ecs_fargate/1- Infrastructure/outputs.tf
@@ -6,26 +6,11 @@ output "vpc_cidr_block" {
   value = aws_vpc.production-vpc.cidr_block
 }
 
-output "public_subnet_1_id" {
-  value = aws_subnet.public-subnet-1.id
+output "public_subnets" {
+  value = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id, aws_subnet.public-subnet-3.id]          
 }
 
-output "public_subnet_2_id" {
-  value = aws_subnet.public-subnet-2.id
+output "private_subnets" {
+  value = [aws_subnet.private-subnet-1.id,aws_subnet.private-subnet-2.id,aws_subnet.private-subnet-3.id]
 }
 
-output "public_subnet_3_id" {
-  value = aws_subnet.public-subnet-3.id
-}
-
-output "private_subnet_1_id" {
-  value = aws_subnet.private-subnet-1.id
-}
-
-output "private_subnet_2_id" {
-  value = aws_subnet.private-subnet-2.id
-}
-
-output "private_subnet_3_id" {
-  value = aws_subnet.private-subnet-3.id
-}
